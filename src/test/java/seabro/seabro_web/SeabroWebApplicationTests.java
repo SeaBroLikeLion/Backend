dc491ca23a5e9372533b5106b89d7e342cd82156
Jongyun Won <jywon1128@gmail.com>
2025-06-28 12:56:56 +0900

    project initialized

---
+++ src/test/java/seabro/seabro_web/SeabroWebApplicationTests.java
@@ -0,0 +1,13 @@
+package seabro.seabro_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeabroWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
